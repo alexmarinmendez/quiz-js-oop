a72acfaf4e73ede99ffac8ef21da5bd2f241fc8c
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-10-03 17:55:35 -0500

    feat: literal object

--- app.js
+++ app.js
@@ -1,4 +1,49 @@
-alert(`Welcome to the NO OOP Quiz!\nYou will see 3 questions.`)
+// alert(`Welcome to the NO OOP Quiz!\nYou will see 3 questions.`)
+
+let question1 = {
+    title: "What is the only thing that computers understand?",
+    answers: ["Machine Code", "High Level Languages", "Low Level Languages", "Algorithms"],
+    correctAnswer : 1,
+    getBody: function() {
+        let body = this.title.toUpperCase() + '\n'
+        for (let i=0; i<this.answers.length; i++) {
+             body += (i+1) + '. ' + this.answers[i] + '\n'
+        }
+        return body
+    },
+    addAnswer: function(answer) {
+        // this.answers[this.answers.length] = answer
+        this.answers.push(answer)
+    },
+    isCorrectAnswer: function(userAnswer) {
+        if (this.correctAnswer == userAnswer) return true
+        else return false
+    }
+}
+let question2 = {
+    title: "What is the only thing that computers understand?",
+    answers: ["Machine Code", "High Level Languages", "Low Level Languages", "Algorithms"],
+    correctAnswer : 1,
+    getBody: function() {
+        let body = this.title.toUpperCase() + '\n'
+        for (let i=0; i<this.answers.length; i++) {
+             body += (i+1) + '. ' + this.answers[i] + '\n'
+        }
+        return body
+    },
+    addAnswer: function(answer) {
+        // this.answers[this.answers.length] = answer
+        this.answers.push(answer)
+    },
+    isCorrectAnswer: function(userAnswer) {
+        if (this.correctAnswer == userAnswer) return true
+        else return false
+    }
+}
+
+question1.addAnswer("C++")
+let userAnswer = prompt(question1.getBody())
+console.log(question1.isCorrectAnswer(userAnswer))
 
 let questions = [
 `What is the only thing that computers understand?
@@ -21,16 +66,16 @@ let questions = [
 let correctResponses = [1, 3, 1]
 let counter = 0
 
-for (let i=0; i<questions.length; i++) {
-    let userResponse = prompt(questions[i])
-    if (userResponse == correctResponses[i]) {
-        console.log('You won! 🥳')
-        counter++
-    } else {
-        console.log('You lost! 😔')
-    }
-}
+// for (let i=0; i<questions.length; i++) {
+//     let userResponse = prompt(questions[i])
+//     if (userResponse == correctResponses[i]) {
+//         console.log('You won! 🥳')
+//         counter++
+//     } else {
+//         console.log('You lost! 😔')
+//     }
+// }
 
-alert(`You got ${counter} points!`)
+// alert(`You got ${counter} points!`)
 
 
